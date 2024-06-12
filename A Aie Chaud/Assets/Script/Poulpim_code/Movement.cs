f537d7f57c7b722d6d071c34586fe9d7a3e4200e
Lyam sauvage <sauvage.lyam@outlook.fr>
2024-06-12 15:54:31 +0200

    perso

--- A Aie Chaud/Assets/Script/Poulpim_code/Movement.cs
+++ A Aie Chaud/Assets/Script/Poulpim_code/Movement.cs
@@ -7,16 +7,11 @@ using UnityEngine.UIElements;
 public class Player : MonoBehaviour
 
 {
-
-    public int speed = 1;
-    public int speed_run = 1;
-    static Animator anim;
     Rigidbody rb;
     BoxCollider controller;
 
 void Start()
     {
-        anim = GetComponent<Animator>();
         rb = GetComponent<Rigidbody>();
     }
 
@@ -28,36 +23,35 @@ void Start()
 
         if (Input.GetKey(KeyCode.Z))
         {
-            transform.Translate(Vector3.forward * speed * Time.deltaTime);
+            transform.Translate(Vector3.forward * 2 * Time.deltaTime);
         }
-
 
         if (Input.GetKey(KeyCode.Z))
         {
             if (Input.GetKey (KeyCode.LeftShift))
             {
-                transform.Translate(Vector3.forward * speed_run * Time.deltaTime);
+                transform.Translate(Vector3.forward * 4 * Time.deltaTime);
             }
         }
 
         if (Input.GetKey(KeyCode.Q))
         {
-            transform.Translate(Vector3.left * speed * Time.deltaTime);
+            transform.Translate(Vector3.left * 2 * Time.deltaTime);
         }
 
         if (Input.GetKey(KeyCode.D))
         {
-            transform.Translate(Vector3.right * speed * Time.deltaTime);
+            transform.Translate(Vector3.right * 2 * Time.deltaTime);
         }
 
         if (Input.GetKey(KeyCode.S))
         {
-            transform.Translate(Vector3.back * speed * Time.deltaTime);
+            transform.Translate(Vector3.back * 2 * Time.deltaTime);
         }
 
         if (Input.GetKey(KeyCode.Space))
         {
-            rb.AddForce(new Vector3(0, (float)0.5, 0), ForceMode.Impulse);
+            rb.AddForce(new Vector3(0, 0.5f, 0), ForceMode.Impulse);
         }
     }
 }
