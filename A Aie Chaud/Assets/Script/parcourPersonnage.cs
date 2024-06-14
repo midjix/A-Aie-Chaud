275d59712374138a91caa4d6b3af2f768dc5e4a6
William Baal <williamxp95@gmail.com>
2024-06-14 16:28:37 +0200

    jeu fini

--- A Aie Chaud/Assets/Script/parcourPersonnage.cs
+++ A Aie Chaud/Assets/Script/parcourPersonnage.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class parcourPersonnage : MonoBehaviour
 {
@@ -20,8 +21,9 @@ public class parcourPersonnage : MonoBehaviour
     {
         if (hit.gameObject.name == "Sphere" || hit.gameObject.name == "Sphere (1)" || hit.gameObject.name == "Sphere (2)" || hit.gameObject.name == "Sphere (3)" || hit.gameObject.name == "Sphere (4)" || hit.gameObject.name == "Sphere (5)" || hit.gameObject.name == "Sphere (6)" || hit.gameObject.name == "Sphere (7)" || hit.gameObject.name == "Sphere (8)" || hit.gameObject.name == "Sphere (9)" || hit.gameObject.name == "Sphere (10)" || hit.gameObject.name == "Sphere (11)" || hit.gameObject.name == "Sphere (12)")
         {
-            Vector3 newPosition = new Vector3(14.57131f, 76.05f, 0.1097848f);
-            this.transform.position = newPosition;
+            //Vector3 newPosition = new Vector3(14.57131f, 76.05f, 0.1097848f);
+            //this.transform.position = newPosition;
+            SceneManager.LoadScene(10);
         }
     }
 }
--- A Aie Chaud/Assets/mouse5.cs
+++ A Aie Chaud/Assets/mouse5.cs
@@ -2,22 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Parcour : MonoBehaviour
+public class mouse5 : MonoBehaviour
 {
-    public float speed = 10;
-    public GameObject personnage;
-    public GameObject spawn;
     // Start is called before the first frame update
     void Start()
     {
-        GetComponent<Rigidbody>().velocity = new Vector3(0, 0, speed);
+        Cursor.visible = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        
     }
-
-    
 }
